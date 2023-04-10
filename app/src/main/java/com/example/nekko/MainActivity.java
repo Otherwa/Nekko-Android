f1fa7f708abd69553658db32585597459fe355d6
Otherwa <67428572+Atharv2002@users.noreply.github.com>
2023-04-10 12:32:40 +0530

    Initial Commit

--- app/src/main/java/com/example/nekko/MainActivity.java
+++ app/src/main/java/com/example/nekko/MainActivity.java
@@ -1,14 +1,39 @@
 package com.example.nekko;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
+import com.google.android.material.tabs.TabLayout;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        // Find the TabLayout view
+        TabLayout tabLayout = findViewById(R.id.tabview);
+
+        // Add tabs to the TabLayout
+        tabLayout.addTab(tabLayout.newTab());
+        tabLayout.addTab(tabLayout.newTab());
+        tabLayout.addTab(tabLayout.newTab());
+
+        // Create a ViewPager adapter to manage the three fragments
+        ViewPager viewPager = findViewById(R.id.viewpager);
+        MyPagerAdapter adapter = new MyPagerAdapter(getSupportFragmentManager());
+        adapter.addFragment(new Pics(), "Pics");
+        adapter.addFragment(new Feed(), "Feed");
+        adapter.addFragment(new Profiles(), "Profile");
+        viewPager.setAdapter(adapter);
+
+        // Link the TabLayout to the ViewPager
+        tabLayout.setupWithViewPager(viewPager);
+
+
     }
 }
---
+++ app/src/main/java/com/example/nekko/MyPagerAdapter.java
@@ -0,0 +1,39 @@
+package com.example.nekko;
+
+import static androidx.fragment.app.FragmentStatePagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT;
+
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MyPagerAdapter extends FragmentPagerAdapter {
+    private final List<Fragment> mFragments = new ArrayList<>();
+    private final List<String> mFragmentTitles = new ArrayList<>();
+
+    public MyPagerAdapter(FragmentManager fm) {
+        super(fm, BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT);
+    }
+
+    public void addFragment(Fragment fragment, String title) {
+        mFragments.add(fragment);
+        mFragmentTitles.add(title);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return mFragments.get(position);
+    }
+
+    @Override
+    public int getCount() {
+        return mFragments.size();
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return mFragmentTitles.get(position);
+    }
+}
